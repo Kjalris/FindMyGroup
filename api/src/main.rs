abe5c2ce0bf6328a5f8bcbe4e8c12be2e2ef7925
frederikstroem <frederikstroem@pm.me>
2021-10-06 11:48:48 +0200

    Add simple api endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{meme}")]
+async fn index(web::Path(meme): web::Path<String>) -> impl Responder {
+    format!("Hello {}!", meme)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
