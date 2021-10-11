954b4e634343773ae0fd751dd6870d53e1fee4cc
frederikstroem <frederikstroem@pm.me>
2021-10-11 13:37:00 +0200

    Add some progress

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,14 +1,54 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+#[macro_use]
+extern crate actix_web;
+#[macro_use]
+extern crate diesel;
 
-#[get("/api/v1/{meme}")]
-async fn index(web::Path(meme): web::Path<String>) -> impl Responder {
-    format!("Hello {}!", meme)
-}
+use std::{env, io};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+use actix_web::{middleware, App, HttpServer};
+use diesel::r2d2::ConnectionManager;
+use diesel::PgConnection;
+use r2d2::{Pool, PooledConnection};
+
+mod constants;
+mod response;
+mod schema;
+mod area;
+mod group;
+mod member;
+mod location;
+
+pub type DBPool = Pool<ConnectionManager<PgConnection>>;
+pub type DBPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    // set up database connection pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    HttpServer::new(move || {
+        App::new()
+            // Set up DB pool to be used with web::Data<Pool> extractor
+            .data(pool.clone())
+            // enable logger - always register actix-web Logger middleware last
+            .wrap(middleware::Logger::default())
+            // register HTTP requests handlers
+            // .service(tweet::list)
+            // .service(tweet::get)
+            // .service(tweet::create)
+            // .service(tweet::delete)
+            // .service(like::list)
+            // .service(like::plus_one)
+            // .service(like::minus_one)
+    })
+    .bind("0.0.0.0:9090")?
+    .run()
+    .await
 }
