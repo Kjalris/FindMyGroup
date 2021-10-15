414ff191748bb43ddf0fb30553ba04c5bd4adad4
frederikstroem <frederikstroem@pm.me>
2021-10-15 13:55:35 +0200

    Add progress

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,17 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder};
+
+// - /groups
+//  - POST: Add a new group, return group entity.
+#[post("/groups")]
+pub async fn create_group(post: web::Json<NewPost>, pool: web::Data<PgPool>) -> impl Responder {
+    println!("New POST request to create a group.");
+    let result = Post::new_post(post.into_inner(), pool.as_ref()).await;
+
+    match result {
+        Ok(post) => HttpResponse::Ok().json(post),
+        _ => HttpResponse::BadRequest().body("Error trying to create a new post")
+    }
+}
 
 #[get("/{meme}")]
 async fn index(web::Path(meme): web::Path<String>) -> impl Responder {
